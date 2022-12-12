6019cf3d1293a850ba46ede309a7995a959b0e70
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2022-12-12 21:54:33 +0530

    Create bottomviewbinarytree.cpp

---
+++ 6 month DSA Practice Sheet/Binary Tree/bottomviewbinarytree.cpp
@@ -0,0 +1,46 @@
+
+/*https://workat.tech/problem-solving/practice/bottom-view-binary-tree*/
+
+/* This is the Node class definition
+
+class Node {
+public:
+    Node* left;
+    Node* right;
+    int data;
+
+    Node(int data) {
+        this->left = NULL;
+        this->right = NULL;
+        this->data = data;
+    }
+};
+*/
+
+vector<int> bottomView(Node* root) {
+    // add your logic here
+	vector<int>ans;
+	if(root==NULL){
+		return ans;
+	}
+	map<int,int>topNode;
+	queue<pair<Node* ,int>>q;
+	q.push(make_pair(root,0));
+	while(!q.empty()){
+		pair<Node*,int>temp=q.front();
+		q.pop();
+		Node* frontNode=temp.first;
+		int hd=temp.second;
+		topNode[hd]=frontNode->data;
+		if(frontNode->left){
+			q.push(make_pair(frontNode->left,hd-1));
+		}
+		if(frontNode->right){
+			q.push(make_pair(frontNode->right,hd+1));
+		}
+	}
+	for(auto i:topNode){
+		ans.push_back(i.second);
+	}
+	return ans;
+}
